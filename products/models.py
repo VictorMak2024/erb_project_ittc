f218d7b11807dacf8e760b89abda18f096e6be06
wcng <nwcn0307@hotmail.com>
2025-04-22 23:19:45 +0800

    step 20: updated shopping cart and add one bit flag for takeOrder

--- products/models.py
+++ products/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
 class Product(models.Model):
     hotitem = models.BooleanField(default=False)
     salesmen = models.ForeignKey(Salesmen, on_delete=models.DO_NOTHING)
-    title = models.ForeignKey(Category, on_delete=models.DO_NOTHING,default=1)
+    title = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
     no = models.CharField(max_length=50)
     operation = models.CharField(max_length=50, choices=operation_choices.items())
     control = models.CharField(max_length=50, choices=control_choices.items())
